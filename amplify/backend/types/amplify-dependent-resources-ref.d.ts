bf6b188c2f9d475eea1eee626f1871a0150cf0a4
Benedict Gattas <gattasb@gmail.com>
2023-01-06 12:30:56 +1000

    Add amplify auth + storage config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "carouselcfc315e5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3carouselstorageeeaac772": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
