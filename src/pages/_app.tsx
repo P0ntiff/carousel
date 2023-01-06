32624ccc085769a9656bf26a14c58684dd553acb
Benedict Gattas <gattasb@gmail.com>
2023-01-06 16:34:18 +1000

    Uncomment auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,9 @@ import "@aws-amplify/ui-react/styles.css";
 import type { AppProps } from "next/app";
 
 // backend setup
-//import { Amplify, Auth, Storage } from "aws-amplify";
-//import awsconfig from "../aws-exports";
-//Amplify.configure({ ...awsconfig, ssr: true });
+import { Amplify, Auth, Storage } from "aws-amplify";
+import awsconfig from "../aws-exports";
+Amplify.configure({ ...awsconfig, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
