c4edf00889a62734e0de359d540d3909759b76ba
Benedict Gattas <gattasb@gmail.com>
2023-01-06 14:51:48 +1000

    Fix aws config import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import '../styles/globals.css'
-import '@aws-amplify/ui-react/styles.css'
+import "../styles/globals.css";
+import "@aws-amplify/ui-react/styles.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 // backend setup
-import { Amplify, Auth, Storage } from 'aws-amplify';
-import awsconfig from '../aws-exports';
-Amplify.configure({...awsconfig, ssr: true});
+import { Amplify, Auth, Storage } from "aws-amplify";
+const awsconfig = require("../aws-exports").default;
+Amplify.configure({ ...awsconfig, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
