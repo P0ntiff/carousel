56f3c45c032698ee54ed983c280a15384d336ce5
Benedict Gattas <gattasb@gmail.com>
2023-02-10 11:40:26 +0000

    Amplify codespaces

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "@aws-amplify/ui-react/styles.css";
 import type { AppProps } from "next/app";
 
 // backend setup
-import { Amplify, Auth, Storage } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import awsconfig from "../aws-exports";
 Amplify.configure({ ...awsconfig, ssr: true });
 
